cacee6b53b3406529fe9080e03c623b819cad1e0
tamr7 <90578823+tamr7@users.noreply.github.com>
2024-10-17 12:22:10 -0300

    mira

--- WindowsApp4/Form1.vb
+++ WindowsApp4/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Inicialización si es necesaria
+    End Sub
 
+    ' Evento para Button1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Cambia el color del fondo de Button2 a azul cuando se presiona Button1
+        Button2.BackColor = Color.Blue
+    End Sub
+
+    ' Evento para Button2 (puedes agregar código adicional si es necesario)
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Puedes agregar código aquí para Button2 si deseas hacer algo cuando se haga clic
+    End Sub
 End Class
+
