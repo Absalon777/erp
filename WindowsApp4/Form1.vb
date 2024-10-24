11c783ea415f83c23b1b933fd646b34c773432a4
Joaquin Sepulveda <joasepbur.02.js@gmail.com>
2024-10-24 18:12:14 -0300

    absi, quiero descargar es

--- WindowsApp4/Form1.vb
+++ WindowsApp4/Form1.vb
@@ -1,21 +1,4 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Inicialización si es necesaria
-    End Sub
 
-    ' Evento para Button1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) 
-        ' Cambia el color del fondo de Button2 a azul cuando se presiona Button1
-        Button2.BackColor = Color.Blue
-    End Sub
-
-    ' Evento para Button2 (puedes agregar código adicional si es necesario)
-    Private Sub Button2_Click(sender As Object, e As EventArgs) 
-        ' Puedes agregar código aquí para Button2 si deseas hacer algo cuando se haga clic
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) 
-        Text = "hola"
-    End Sub
 End Class
 
